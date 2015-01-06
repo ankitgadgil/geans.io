d316f32d8fc4c9777ea2a4585e535e09b3f53dae
Ankit <ankit>
2015-01-06 16:28:13 +0530

    base commit for up system

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/bikestore/admin/');
+define('HTTP_CATALOG', 'http://localhost/bikestore/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/bikestore/admin/');
+define('HTTPS_CATALOG', 'http://localhost/bikestore/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/bikestore/admin/');
+define('DIR_SYSTEM', '/var/www/html/bikestore/system/');
+define('DIR_DATABASE', '/var/www/html/bikestore/system/database/');
+define('DIR_LANGUAGE', '/var/www/html/bikestore/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/bikestore/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/bikestore/system/config/');
+define('DIR_IMAGE', '/var/www/html/bikestore/image/');
+define('DIR_CACHE', '/var/www/html/bikestore/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/bikestore/download/');
+define('DIR_LOGS', '/var/www/html/bikestore/system/logs/');
+define('DIR_CATALOG', '/var/www/html/bikestore/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'bike');
+define('DB_PASSWORD', 'bike123');
+define('DB_DATABASE', 'bikestore');
+define('DB_PREFIX', 'bs_');
+?>
