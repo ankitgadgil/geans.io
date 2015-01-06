0b7e379381354722e6c3cc00c060464a18e498fe
Ankit <ankit>
2015-01-06 16:33:09 +0530

    database fix on configuration

--- admin/config.php
+++ admin/config.php
@@ -1,30 +1,30 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/bikestore/admin/');
-define('HTTP_CATALOG', 'http://localhost/bikestore/');
+define('HTTP_SERVER', 'http://localhost/geans.io/admin/');
+define('HTTP_CATALOG', 'http://localhost/geans.io/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/bikestore/admin/');
-define('HTTPS_CATALOG', 'http://localhost/bikestore/');
+define('HTTPS_SERVER', 'http://localhost/geans.io/admin/');
+define('HTTPS_CATALOG', 'http://localhost/geans.io/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/bikestore/admin/');
-define('DIR_SYSTEM', '/var/www/html/bikestore/system/');
-define('DIR_DATABASE', '/var/www/html/bikestore/system/database/');
-define('DIR_LANGUAGE', '/var/www/html/bikestore/admin/language/');
-define('DIR_TEMPLATE', '/var/www/html/bikestore/admin/view/template/');
-define('DIR_CONFIG', '/var/www/html/bikestore/system/config/');
-define('DIR_IMAGE', '/var/www/html/bikestore/image/');
-define('DIR_CACHE', '/var/www/html/bikestore/system/cache/');
-define('DIR_DOWNLOAD', '/var/www/html/bikestore/download/');
-define('DIR_LOGS', '/var/www/html/bikestore/system/logs/');
-define('DIR_CATALOG', '/var/www/html/bikestore/catalog/');
+define('DIR_APPLICATION', '/var/www/html/geans.io/admin/');
+define('DIR_SYSTEM', '/var/www/html/geans.io/system/');
+define('DIR_DATABASE', '/var/www/html/geans.io/system/database/');
+define('DIR_LANGUAGE', '/var/www/html/geans.io/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/geans.io/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/geans.io/system/config/');
+define('DIR_IMAGE', '/var/www/html/geans.io/image/');
+define('DIR_CACHE', '/var/www/html/geans.io/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/geans.io/download/');
+define('DIR_LOGS', '/var/www/html/geans.io/system/logs/');
+define('DIR_CATALOG', '/var/www/html/geans.io/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'bike');
 define('DB_PASSWORD', 'bike123');
-define('DB_DATABASE', 'bikestore');
+define('DB_DATABASE', 'geans.io');
 define('DB_PREFIX', 'bs_');
 ?>
